521c6421cd619c13a4b656202d2fe05b4ff8816c
Rahul Sharma <rg4142451@gmail.com>
2025-03-16 12:49:08 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Weather from './Weather.jsx'
+
+createRoot(document.getElementById('root')).render(
+    <App />
+    // <Weather/>
+)
